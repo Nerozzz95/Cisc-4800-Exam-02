1015f54250a98fdc83e89d97cad56f96b3e73dad
Nerozzz95 <64553939+Nerozzz95@users.noreply.github.com>
2020-12-01 22:38:38 -0500

    Update Script1.js

--- Cisc 4800 Exam 02/Script1.js
+++ Cisc 4800 Exam 02/Script1.js
@@ -1,16 +1,21 @@
 // JavaScript source code
 function fetchData() {
-    const getData = Math.floor(Math.random());
-    fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data.all[getData]);
-            const get = data.all[getData];
-            document.getElementById("Year").innerHTML = "#" + getData;
-            document.getElementById("Deaths").innerHTML = get.text;
-            document.getElementById("Death Rate").innerHTML = getData + "%";
-        })
-        .catch(error => console.log("ERROR"));
+    return fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+        .then(response => response.json())
+        .then(json => renderDatas(json))
+        .catch(error => console.log("ERROR"))
 }
+
+function renderDatas(json){
+    const main = document.creatElement('p')
+    json.forEach(data => {
+    p.innerHTML = <strong>Year:</strong> ${data.year}
+    p.innerHTML = <strong>Deaths:</strong> ${data.death}
+    p.innerHTML = <strong>Death Rate:</strong> ${data.death_rate}
+    main.appendData(p)
+   })
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchData()
+})
